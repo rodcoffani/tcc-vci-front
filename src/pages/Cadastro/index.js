b6132b53a7ac85528ffb823a74d0c3c736b6948d
Jose-Estevao <Jose-Estevao@users.noreply.github.com>
2020-08-13 20:58:56 -0300

    Partículas

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,26 +1,111 @@
-import React, { Component } from 'react';
-import { Container, Row, Col} from 'react-bootstrap'
-
+import React from 'react';
 import './styles.css';
+import { Container, Row, Col} from 'react-bootstrap'
+import { Component } from "react"
+import {Helmet} from "react-helmet"
+import Particles from 'react-particles-js'; 
 
-export default class Saudacao extends Component {
+export default class Saudacao extends Component{
     state={
         nome: this.props.nome
     }
     setNome(e){
         this.setState({ nome:e.target.value})
     }
-    render() {
+    render(){
         const {nome} = this.state
         return (
-                <React.Fragment>
-                <body className="Cont">
+                
+            <React.Fragment>
+                
+                <Helmet title="Cadastro" />
+                
                 <Container fluid="xl">
+                <Row className="TopShelf"><h1 className="CadT">CADASTRO</h1>   
+                </Row>
+                <hr/>
+                <Particles className="Part"
+                        params={{ 
+                            "particles": {
+                                "number": {
+                                    "value": 60,
+                                    "density": {
+                                        "enable": true,
+                                        "value_area": 1000
+                                    }
+                                },
+                                "color": {
+                                    "value": "#f00080"
+                                },
+                                "shape": {
+                                    "type": "circle",
+                                    "stroke": {
+                                        "width": 2,
+                                        "color": "#3030ff"
+                                    },
+                                    "polygon": {
+                                        "nb_sides": 3
+                                    },
+                                },
+                                "opacity": {
+                                    "value": 0.4008530152163807,
+                                    "random": false,
+                                    "anim": {
+                                        "enable": false,
+                                        "speed": 1,
+                                        "opacity_min": 0.1,
+                                        "sync": false
+                                    }
+                                },
+                                "line_linked": {
+                                    "enable": true,
+                                    "color": "#ffffff",
+                                    "width": 0.6413648243462091
+                                },
+                                "size": {
+                                    "value": 1.5,
+                                    "random": true,
+                                    "anim": {
+                                        "enable": false,
+                                        "speed": 40,
+                                        "size_min": 0.1,
+                                        "sync": false
+                                    }
+                                },
+                                "move": {
+                                    "enable": true,
+                                    "speed": 6,
+                                    "direction": "none",
+                                    "random": true,
+                                    "straight": false,
+                                    "out_mode": "out",
+                                    "bounce": false,
+                                    "attract": {
+                                        "enable": false,
+                                        "rotateX": 600,
+                                        "rotateY": 1200
+                                    }
+                                }
+                            },
+                            "interactivity": {
+                                "detect_on": "window",
+                                "events": {
+                                    "onhover": {
+                                        "enable": true,
+                                        "mode": "grab"
+                                    },
+                                    "resize": true
+                                },
+                            },
+                            "retina_detect": true
+                    
+                         }} 
+                /> 
                 <Row>
                 <Col><div className="TituloC">
-                    <p className="CadT">CADASTRO</p>
                     <p className="CadS">Se inscreva e seja bem-vindo, {nome}<b className="Flick">_</b>! ;)</p>
-                    </div></Col>
+                    </div>
+                </Col>
                 <Col>
                     <div className="FormC">
                     <form>
@@ -33,11 +118,16 @@ export default class Saudacao extends Component {
                     </div>
                 </Col>
                 </Row>
-                
+                <hr/>
                 <a href="https://ead-lab.coursify.me" className="linkC">https://ead-lab.coursify.me/</a>
                 </Container>
-                </body>
                 </React.Fragment>
-              );        
+                
+              
+              
+        
+              );
+
+        
     }
 }
