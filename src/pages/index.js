8173c2bb87e84b2af9d90019746821e8b62ed85d
Kaio Henrique Avelino Domeni de Paulo <henriquekaio@live.com>
2020-09-16 16:26:23 -0300

    Alteração na div maior da home, mudanças no carrossel e alteração na disposição dos elementos

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ function App() {
     <Sidebar pageSelected="reports"/>
       <Header headerTitle="Home"/>
       <div className="content">
-        <Container style={{width:"80vw", marginTop:"30px"}}>
+        <Container fluid="xl" style={{width:"85vw", marginTop:"2.5vw", marginBottom:"3.5vw"}}>
         <backgroundParticle></backgroundParticle>
         <Carousel style={{height:"20vw"}}>
           <Carousel.Item>
@@ -31,7 +31,7 @@ function App() {
               alt="P. slide"
             />
             <Carousel.Caption className='Caption'>
-              <h1 style={{fontWeight:"bold"}}>Sorteio de questões</h1>
+              <h1 style={{fontWeight:"bold", fontSize:"3vw"}}>Sorteio de questões</h1>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
@@ -42,7 +42,7 @@ function App() {
             />
 
             <Carousel.Caption className='Caption'>
-              <h1 style={{fontWeight:"bold"}}>Forca</h1>
+              <h1 style={{fontWeight:"bold", fontSize:"3vw"}}>Forca</h1>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
@@ -53,23 +53,23 @@ function App() {
             />
 
             <Carousel.Caption className='Caption'>
-              <h1 style={{fontWeight:"bold"}}>Comparação de resultados</h1>
+              <h1 style={{fontWeight:"bold", fontSize:"3vw"}}>Comparação de resultados</h1>
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
 
       <div className="mid">
         <Row>
-            <Col style={{float:"left", paddingTop:"50px"}}>
+            <Col style={{float:"left", paddingTop:"3.5vw"}}>
             <img src={home_mid} style={{height:"35vw"}}/>
             </Col>
-            <Col style={{paddingTop:"70px", textAlign:"justify", paddingRight:"70px", fontSize:"1.2vw"}}>
+            <Col style={{paddingTop:"5vw", paddingRight:"13vw", fontSize:"1.2vw"}}>
             <h3>O projeto</h3>
             <p>O EAD-LAB é uma plataforma de jogos didáticos que têm como objetivo fazer com que o aprendizado seja uma atividade atrativa e prazerosa. Fornecendo uma experiência que é, ao mesmo tempo, confortável e instrutiva, nossos jogos são concebidos no intuito de facilitar a assimilação de conteúdos com uma dinâmica elaborada para expandir e reforçar o desenvolvimento profissional dos usuários.</p>
             
-            <ul style={{listStyle:"none", marginLeft:"-42px"}}>
-              <li><FontAwesomeIcon icon={faCheckCircle}/> Combinação perfeita entre educação e entretenimento;</li>
+            <ul style={{listStyle:"none", marginLeft:"-7.8vw"}}>
               <li><FontAwesomeIcon icon={faCheckCircle}/> Testa as capacidades de se trabalhar em equipe;</li>
+              <li><FontAwesomeIcon icon={faCheckCircle}/> Educação e entretenimento combinados;</li>
               <li><FontAwesomeIcon icon={faCheckCircle}/> Procedimento rápido e intuitivo;</li>
               <li><FontAwesomeIcon icon={faCheckCircle}/> Viável e portátil.</li>
             </ul>
@@ -79,21 +79,21 @@ function App() {
         
           <Row >
               <Col>
-              <span style={{fontSize:"3.5vw"}}>Nossos jogos!</span>
+              <span style={{fontSize:"3vw"}}>Nossos jogos!</span>
               </Col>
           </Row>
           <Row>
           <Col xs={1} md={1}></Col>
           <Col xs={10} md={10}>
           <CardGroup style={{
-          marginTop:'78px',
-          marginBottom:'20px'
+          marginTop:'6vw',
+          marginBottom:'6vw'
           }} className='EfctvCardG'>
             <Card className='EfctvCard'>
               <Card.Img variant="top" src="" />
               <Card.Body>
                 <Card.Title style={{textAlign:'center', fontSize:"1.3vw"}}>Título</Card.Title>
-                <Card.Text>
+                <Card.Text style={{fontSize:"1.1vw", marginLeft:"0.1vw"}}>
                   1.
                 </Card.Text>
               </Card.Body>
@@ -109,7 +109,7 @@ function App() {
               <Card.Img variant="top" src="" />
               <Card.Body>
                 <Card.Title style={{textAlign:'center', fontSize:"1.3vw"}}>Título</Card.Title>
-                <Card.Text>
+                <Card.Text style={{fontSize:"1.1vw", marginLeft:"0.1vw"}}>
                   2.
                 </Card.Text>
               </Card.Body>
@@ -125,7 +125,7 @@ function App() {
               <Card.Img variant="top" src="" />
               <Card.Body>
                 <Card.Title  style={{textAlign:'center', fontSize:"1.3vw"}}>Título</Card.Title>
-                <Card.Text>
+                <Card.Text style={{fontSize:"1.1vw", marginLeft:"0.1vw"}}>
                   3.
                 </Card.Text>
               </Card.Body>
@@ -141,12 +141,12 @@ function App() {
           </Col>
           </Row>
           </div>
-        </Container>
-      </div>
-      <a style={{textAlign:'center', cursor:'pointer',color:'white'}} onClick={()=>scroll.scrollToTop()} >
+          <a style={{textAlign:'center', cursor:'pointer',color:'white'}} onClick={()=>scroll.scrollToTop()} >
       <div style={{backgroundColor:'black'}} className='Topo'>
       <FontAwesomeIcon icon={faCaretSquareUp} size="lg" />
       </div></a>
+        </Container>
+      </div>
   </React.Fragment>
     
   );
