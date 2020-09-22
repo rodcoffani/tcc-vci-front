177e2045abed0f9bc397b6b74b2693c5282761ae
Rodrigo Coffani <rodrigo.coffani@gmail.com>
2020-09-22 15:59:52 -0300

    buscando lista de usuários

--- src/pages/Administrador/index.js
+++ src/pages/Administrador/index.js
@@ -1,121 +1,111 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
 import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar/admin';
 import { Container, Row, Col} from 'react-bootstrap'
-import {Helmet} from "react-helmet"
 import BackgroundParticle from '../../components/Background-particle'
 import API from "../../api";
-import $ from "jquery";
 
-class admin extends Component {
-    constructor(props){
-        super(props);
-        const users = this.selectUsers();
-    }
+const Admin = (props) => {
+    const [users, setUsers] = useState([]);
 
-    selectUsers(){
-        const tk = {
-            token: localStorage.getItem('authTk')
-        };
-        if (tk) {
-            API.get("/users/all", tk).then((res) => {
-                console.log(res.data);
-                return res.data;
-            });
-        }
-    }
+    useEffect(() => {
+        API.get("/users/all").then((res) => {
+            setUsers(res.data);
+        });
+    }, []);
     
-    render() {
-        return(
-            <div>
-                <Sidebar pageSelected="reports"/>
-                <Header headerTitle="Administrador"/>
-                <React.Fragment>
-                <Container fluid="xl">
-                    <BackgroundParticle></BackgroundParticle>
-                </Container>
-                <center>
-                    <div className="mother">
-                        <br />
-                        <div className="ranking">
-                            <div className="ranking-title">
-                                <div className="ranking-title-combo">
-                                    Filtrar por jogo: <br />
-                                    <select>
-                                        <option value="a">Quiz</option>
-                                        <option value="b">Perguntados</option>
-                                    </select>
-                                </div> 
-                                <div className="ranking-title-title">
-                                    <p className="ranking-text">Ranking</p>
-                                </div>
-                            </div>
-                            <br /><br /><br /><br />
-                            <div className="ranking-body">
-                                <table class="table table-striped header-fixed">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Nome</th>
-                                        <th scope="col">Pontos</th>
-                                        <th scope="col">Tempo</th>
-                                        <th scope="col">Jogo</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {/* a fazer: colocar infos na tabela por "for" ? puxando do banco */}
-                                    <tr>
-                                        <td>1</td>
-                                        <td>Mark</td>
-                                        <td>20</td>
-                                        <td>20s</td>
-                                        <td>Quiz</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>Mark</td>
-                                        <td>20</td>
-                                        <td>20s</td>
-                                        <td>Quiz</td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td>Mark</td>
-                                        <td>20</td>
-                                        <td>20s</td>
-                                        <td>Quiz</td>
-                                    </tr>
-                                </tbody>
-                                </table>
+    return (
+        <div>
+            <Sidebar pageSelected="reports"/>
+            <Header headerTitle="Administrador"/>
+            <React.Fragment>
+            <Container fluid="xl">
+                <BackgroundParticle />
+            </Container>
+            <center>
+                <div className="mother">
+                    <br />
+                    <div className="ranking">
+                        <div className="ranking-title">
+                            <div className="ranking-title-combo">
+                                Filtrar por jogo: <br />
+                                <select>
+                                    <option value="a">Quiz</option>
+                                    <option value="b">Perguntados</option>
+                                </select>
+                            </div> 
+                            <div className="ranking-title-title">
+                                <p className="ranking-text">Ranking</p>
                             </div>
                         </div>
-                        <div className="employee">
-                            <div className="employee-title">
-                                <p className="employee-text">Funcionários</p>
-                            </div>
-                            <div className="employee-body">
-                                <table class="table table-striped header-fixed-employee">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Nome</th>
-                                        <th scope="col">Validação</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        {this.users}
-                                    </tr>
-                                </tbody>
-                                </table>
-                            </div>
+                        <br /><br /><br /><br />
+                        <div className="ranking-body">
+                            <table class="table table-striped header-fixed">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">Pontos</th>
+                                    <th scope="col">Tempo</th>
+                                    <th scope="col">Jogo</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {/* a fazer: colocar infos na tabela por "for" ? puxando do banco */}
+                                <tr>
+                                    <td>1</td>
+                                    <td>Mark</td>
+                                    <td>20</td>
+                                    <td>20s</td>
+                                    <td>Quiz</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Mark</td>
+                                    <td>20</td>
+                                    <td>20s</td>
+                                    <td>Quiz</td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Mark</td>
+                                    <td>20</td>
+                                    <td>20s</td>
+                                    <td>Quiz</td>
+                                </tr>
+                            </tbody>
+                            </table>
                         </div>
                     </div>
-                </center>
-                </React.Fragment>
-            </div>
-        );
-    }
+                    <div className="employee">
+                        <div className="employee-title">
+                            <p className="employee-text">Funcionários</p>
+                        </div>
+                        <div className="employee-body">
+                            <table class="table table-striped header-fixed-employee">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">Validação</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {users.map((item) => {
+                                    return <tr>
+                                        <td>{item.name_user}</td>
+                                        <td>{item.iduser}</td>
+                                    </tr>
+                                })}
+                            </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </center>
+            </React.Fragment>
+        </div>
+    );
 };
 
-export default admin;
+export default Admin;
