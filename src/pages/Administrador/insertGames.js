b94b5838b87f35cd92ed5a8bb36305f8eeb2a08e
Graziele <graziele.garcia@unesp.br>
2020-09-29 22:49:57 -0300

    alterações na pagina de cadastri de jogos para adicionar campos

--- src/pages/Administrador/insertGames.js
+++ src/pages/Administrador/insertGames.js
@@ -6,6 +6,41 @@ import { Container, Row, Col, Form, InputGroup, FormControl, FormLabel} from 're
 import {Helmet} from "react-helmet"
 import BackgroundParticle from '../../components/Background-particle'
 import addQuestion from '../../assets/images/mais.PNG'
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import '@fortawesome/fontawesome-free';
+import FlipMove from "react-flip-move"
+
+function ListItems(props){
+    const items = props.items;
+    const listItems = items.map(item =>
+        {
+            //vai criar uma class (?) p cada item
+            //o key acha o item 
+            return <div className="list" key={item.key}>
+                <FormControl 
+                    type="text" 
+                    className = "input" 
+                    id={item.key} 
+                    value={item.text}
+                    onChange={
+                        (e) => {
+                            props.setUpdate(e.target.value, item.key)
+                        }
+                    }
+                />
+                <FontAwesomeIcon icon={faTrashAlt} className="icons" onClick={ () => props.deleteItem(item.key)}/> 
+            </div>
+        })
+    return(
+        //flip move da o efeito quando exclui e adiciona o item
+        <div>
+            <FlipMove duration={300} easing="ease-in-out">
+                {listItems}
+            </FlipMove>
+        </div>
+    )
+}
 
 class insertGames extends Component {
     constructor(props){
@@ -17,21 +52,27 @@ class insertGames extends Component {
                 key: ''
             }
         }
-        this.handleInput = this.handleInput.bind(this);
+        this.handleInput = this.handleInput.bind(this); // para nao dar erro no this das outras funções
         this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+        this.setUpdate = this.setUpdate.bind(this);
     }
+
     handleInput(e){
         this.setState({
-            text: e.target.value,
-            key: Date.now()
+            currentItem:{
+                text: e.target.value, //pega o value do input que escrevemos alguma coisa
+                key: Date.now() 
+            }
         })
     }
+
     addItem(e){
-        e.preventDefault();
+        e.preventDefault(); //para nao fazer um refresh na página (que é o padrão - default - de um botão)
         const newItem = this.state.currentItem;
         console.log(newItem);
         if(newItem.text !== ""){
-            const newItems = [...this.state.items, newItem];
+            const newItems = [...this.state.items, newItem]; //adiciona o newItem em uma array list
             this.setState({
                 items: newItems,
                 currentItem:{
@@ -41,6 +82,28 @@ class insertGames extends Component {
             })
         }
     }
+
+    deleteItem(key){
+        const filteredItems= this.state.items.filter(item => item.key!==key); //filtra os itens que a key não é a mesma e guarda na variavel
+        this.setState({
+            items: filteredItems
+        })
+    }
+
+    setUpdate(text, key){
+        console.log("items:"+this.state.items);
+        const items = this.state.items;
+        items.map(item => {      
+            if(item.key===key){
+                console.log(item.key +"    "+key)
+                item.text= text;
+            }
+        })
+        this.setState({
+            items: items
+        }) 
+    }
+
     render() {
         return(
             <div>
@@ -54,7 +117,7 @@ class insertGames extends Component {
                     <div className="mother">
                         <br />
                         <div className="insert-games">
-                            <Form id="form-label" onSubmit={this.addItem}>
+                            <Form id="to-do-form" onSubmit={this.addItem}>
                                 <div className="labels-insert">
                                     <FormLabel>Selecionar tipo de jogo:</FormLabel><br />
                                     <FormLabel>Nome do jogo:</FormLabel>
@@ -66,9 +129,9 @@ class insertGames extends Component {
                                         <option value="b" className = "select-jogos">Quiz</option>
                                     </FormControl>
                                     <FormControl type="text" placeholder="Digite o nome do jogo" className = "input"/>
-                                    <FormControl type="text" placeholder="Digite o enunciado da pergunta" className = "input"/>
+                                    <FormControl type="text" placeholder="Digite o enunciado da pergunta" className = "input" value={this.state.currentItem.text} onChange={this.handleInput}/>
                                   
-                                    <InputGroup className="mb-3">
+                                    <InputGroup>
                                         <InputGroup.Prepend>
                                             <InputGroup.Radio aria-label="Radio button for following text input" />
                                         </InputGroup.Prepend>
@@ -79,28 +142,33 @@ class insertGames extends Component {
                                         <InputGroup.Prepend>
                                             <InputGroup.Radio aria-label="Radio button for following text input" />
                                         </InputGroup.Prepend>
-                                        <FormControl type="text" placeholder="Digite uma alternativa de resposta" className = "input"/>
+                                        <FormControl type="text" placeholder="Digite uma alternativa de resposta" className = "input" value={this.state.currentItem.text} onChange={this.handleInput}/>
                                     </InputGroup>
 
                                     <InputGroup>
                                         <InputGroup.Prepend>
                                             <InputGroup.Radio aria-label="Radio button for following text input" />
                                         </InputGroup.Prepend>
-                                        <FormControl type="text" placeholder="Digite uma alternativa de resposta" className = "input"/>
+                                        <FormControl type="text" placeholder="Digite uma alternativa de resposta" className = "input" value={this.state.currentItem.text} onChange={this.handleInput}/>
                                     </InputGroup>
 
                                     <InputGroup>
                                         <InputGroup.Prepend>
                                             <InputGroup.Radio aria-label="Radio button for following text input" />
                                         </InputGroup.Prepend>
-                                        <FormControl type="text" placeholder="Digite uma alternativa de resposta" className = "input"/>
+                                        <FormControl type="text" placeholder="Digite uma alternativa de resposta" className = "input" value={this.state.currentItem.text} onChange={this.handleInput}/>
                                     </InputGroup>
 
-                                    <input type="image" src={addQuestion}  className="add-question" alt="submit" name="submit"/>
+                                    <input type="submit" className="add-question" value="+"/>
+                                    <ListItems 
+                                        items = {this.state.items}
+                                        deleteItem = {this.deleteItem}
+                                        setUpdate = {this.setUpdate}>
+                                    </ListItems>  
                             </div>
-                            </Form>    
+                            </Form>  
+                            
                         </div>
-
                         <br />
                     </div>
                 </center>
