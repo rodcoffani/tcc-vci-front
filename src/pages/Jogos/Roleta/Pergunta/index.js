efb8b173134d5e1effab4f7cc30ff7d344f8e2df
Vinicius Prado <vinicavagna@gmail.com>
2020-10-22 15:48:15 -0300

    Mais coisas da roleta

--- src/pages/Jogos/Roleta/Pergunta/index.js
+++ src/pages/Jogos/Roleta/Pergunta/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./styles.css";
 import API from "../../../../api";
 import { useParams } from "react-router";
+import { Alert } from "react-bootstrap";
 
 class Pergunta extends Component {
     constructor(props) {
@@ -9,7 +10,8 @@ class Pergunta extends Component {
         this.state = {
             questao: "",
             alternativas: [],
-            selecionada : 0
+            selecionada: 0,
+            correta : 0
         };
     }
     componentDidMount() {
@@ -17,12 +19,23 @@ class Pergunta extends Component {
         const perguntas = [];
         API.get(`/perguntados/pergunta/${id}`).then((res) => {
             console.log(res.data.data[0].json_question);
+            var flag = 1;
             for (var [key, value] of Object.entries(
                 res.data.data[0].json_question.respostas
             )) {
                 for (var [key1, value1] of Object.entries(value)) {
                     if (key1 != "certa") {
                         perguntas.push(value1);
+                    }
+                    if(key1 == "certa"){
+                        if(value1 == "true"){
+                            console.log("A flag vale "+  flag);
+                            this.setState({
+                                correta : flag
+                            });
+                        }else{
+                            flag++;
+                        }
                     }
                 }
             }
@@ -33,9 +46,12 @@ class Pergunta extends Component {
         });
         console.log(perguntas);
     }
-    handleChange = (e) => {
-
-    }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        if(this.state.selecionada == this.state.correta){
+            alert("acertou");
+        }
+    };
 
     render() {
         let ops = this.state.alternativas;
@@ -50,15 +66,20 @@ class Pergunta extends Component {
                                     <div class="input-group-prepend">
                                         <div class="input-group-text">
                                             <input
-                                                value = {key}
-                                                name = "op"
+                                                value={key}
+                                                name="op"
                                                 type="radio"
+                                                required
                                                 aria-label="Radio button for following text input"
-                                                onChange = {(e)=>{
+                                                onChange={(e) => {
                                                     this.setState({
-                                                        selecionada : e.target.value
+                                                        selecionada:
+                                                        parseInt(e.target.value)+1,
                                                     });
-                                                    console.log(this.state.selecionada);
+                                                    console.log(this.state.correta);
+                                                    console.log(
+                                                        this.state.selecionada
+                                                    );
                                                 }}
                                             />
                                             {value}
@@ -68,7 +89,13 @@ class Pergunta extends Component {
                             </div>
                         );
                     })}
-                    <button className="btn btn-success">ENVIAR</button>
+                    <button
+                     className="btn btn-success"
+                     onClick = {(e) => {
+                        this.handleSubmit(e);
+                    }}
+                    >ENVIAR
+                    </button>
                 </form>
             </React.Fragment>
         );
