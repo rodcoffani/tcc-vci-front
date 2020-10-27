9b0048311d62deb2a82d9faf155d47523a8da7bd
Vinicius Prado <vinicavagna@gmail.com>
2020-10-27 17:44:26 -0300

    Ainda fazendo queue roleta

--- src/pages/Jogos/Roleta/queue.js
+++ src/pages/Jogos/Roleta/queue.js
@@ -5,10 +5,31 @@ class Queue extends Component{
     constructor(props) {
         super(props);
         this.state = {
-
+            conexao : null
         };
     }
+    componentDidMount(){
+        this.setState({
+            conexao : socketIOClient("http://localhost:4000")
+        });
+        var cache = [];
+        var teste = JSON.stringify(this.state.conexao, (key, value) => {
+            if (typeof value === 'object' && value !== null) {
+                // Duplicate reference found, discard key
+                if (cache.includes(value)) return;
+
+                // Store value in our collection
+                cache.push(value);
+               console.log(cache);
+            }
+            return value;
+        });
+        localStorage.setItem("conPlayer", teste);
+    }
+
     render(){
+        console.log(this.state.conexao);
+
         return(
             <h1>OI</h1>
         );
