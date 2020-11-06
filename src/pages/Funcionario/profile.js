cc01c5f2df138ba2aaf6e79605b6302916df2575
Ana Julia <anajuliamarcelino02@gmail.com>
2020-11-06 16:06:55 -0300

    atualização da pagina de funcionário - novo card

--- src/pages/Funcionario/profile.js
+++ src/pages/Funcionario/profile.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import './styles.css';
 import Header from '../../components/Header';
 import Sidebar from '../../components/Sidebar/employee';
@@ -6,9 +6,99 @@ import Foto from '../../assets/images/nn.jpg';
 import { Container, FormControl, Form, Image} from 'react-bootstrap';
 import {Helmet} from "react-helmet";
 import BackgroundParticle from '../../components/Background-particle';
+import API from "../../api";
+import { faEdit, faCheckCircle, faFileUpload} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import '@fortawesome/fontawesome-free';
 
-class employee extends Component{
-    render() {
+const Profile = (props) => {
+    const [admin, setAdmin] = useState([]);
+    const [employee, setEmployee] = useState([]);
+    const [user, setUser] = useState({});
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [id, setId] = useState("");
+    const [profileImg, setImg] = useState("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png");
+    const [reloadFlag, setReloadFlag] = useState(true); 
+
+    useEffect(() => {
+        API.post("/login/teste-token",{token: localStorage.getItem('authTk')}).then((res) => {
+            setUser(res.data.decoded);  
+            setId(res.data.decoded.iduser);
+            setNome(res.data.decoded.name_user);
+            setEmail(res.data.decoded.email_user);
+        });
+    }, []);
+
+    useEffect(() => {
+        API.get("/users/admins").then((res) => {
+            setAdmin(res.data);
+        });
+        API.get("/users/employee").then((res) => {
+            setEmployee(res.data);
+        });
+    }, [reloadFlag]);
+
+    const enableForm = (e) => {
+        e.preventDefault();
+        const inputs = document.getElementsByClassName("input-profile");
+        for(let i=0; i<inputs.length; i++){
+            if(i === 0 || i === 3)
+                continue;
+            inputs.item(i).disabled = false;
+        }
+        inputs.item(1).focus();
+
+        const salvar = document.getElementsByClassName("edit-employee");
+        salvar.item(0).hidden = false;
+
+        const imagem = document.getElementsByClassName("profile-img");
+        imagem.item(0).hidden = false;
+    }
+
+    const editAdmin = (e) => {
+        e.preventDefault();
+        console.log(id, email, nome);
+        API.put("/users/update-user", {id, nome, email}).then((res) => {
+            alert(res.data.message);
+            console.log(res.data);
+        })
+
+        const inputs = document.getElementsByClassName("input-profile");
+        for(let i=0; i<inputs.length; i++){
+            if(i === 0 || i === 3)
+                continue;
+            inputs.item(i).disabled = true;
+        }
+
+        const salvar = document.getElementsByClassName("edit-employee");
+        salvar.item(0).hidden = true;
+
+        const imagem = document.getElementsByClassName("profile-img");
+        imagem.item(0).hidden = true;
+    }
+
+    const imageHandler = (e) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+          if(reader.readyState === 2){
+            setImg(reader.result);
+          }
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    }
+
+    const promoteUser = (email, name) => { 
+        // eslint-disable-next-line no-restricted-globals
+        if(confirm("Deseja salvar as alterações?")){
+            API.put(`users/promote-admin/${email}`, {name}).then((res) => {
+                alert(res.data.message);
+                setReloadFlag(!reloadFlag);
+            });
+        } 
+    }
+
+    // render() {
         return(
             <div>
                 <Helmet title="Perfil" />
@@ -21,7 +111,59 @@ class employee extends Component{
                     <center>
                         <div className="mother">
                             <br />
-                            <div className="ranking">
+                            <div className="row container d-flex card-user-employee">
+                                <Form onSubmit={editAdmin}>
+                                    <div className="col-md-12">
+                                        <div className="card user-card-full">
+                                            <div className="row m-l-0 m-r-0">
+                                                <div className="col-sm-4 bg-c-lite-green user-profile">
+                                                    <div className="card-block-e text-center text-white">
+                                                        <div className="m-b-25">
+                                                            <img src={profileImg} className="img-radius-e"></img>
+                                                            <input type="file" id="photo" hidden onChange={imageHandler} />
+                                                            <label for="photo" className="profile-img" hidden>
+                                                                <FontAwesomeIcon icon={faFileUpload} className="icon-file"/>
+                                                                &nbsp; Fazer upload:
+                                                            </label>
+                                                        </div>
+                                                        <h6 className="f-w-800">{user.name_user}</h6>
+                                                        <FontAwesomeIcon icon={faEdit} className="icon-edit" onClick={enableForm}/>
+                                                    </div>
+                                                </div>
+                                                <div className="col-sm-8">
+                                                    <div className="card-block-e">
+                                                        <h5 className="m-b-20 p-b-5 b-b-default f-w-600">Meu Perfil</h5>
+                                                        <div className="row">
+                                                            <div className="col-sm-12">
+                                                                <p className="m-b-10 f-w-600">Nome de usuário</p>
+                                                                <input type="text" className="input-profile" disabled placeholder={user.nickname_user}></input>
+                                                            </div>
+                                                            <div className="col-sm-12">
+                                                                <p className="m-b-10 f-w-600">Nome</p>
+                                                                <input type="text" className="input-profile" disabled value={nome} onChange={e => setNome(e.target.value)}></input>
+                                                            </div>
+                                                            <div className="col-sm-12">
+                                                                <p className="m-b-10 f-w-600">Email</p>
+                                                                <input type="text" className="input-profile" disabled value={email} onChange={e => setEmail(e.target.value)}></input>
+                                                            </div>
+                                                            <div className="col-sm-12">
+                                                                <p className="m-b-10 f-w-600">CPF</p>
+                                                                <input type="text" className="input-profile" disabled placeholder={user.cpf_user}></input>
+                                                            </div>
+
+                                                            <button type="submit" className="edit-employee" alt="submit" hidden>
+                                                                <FontAwesomeIcon icon={faCheckCircle} className="icon-save"/>
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Form>
+                            </div>
+                            <br />
+                            <div className="ranking-employee">
                                 <div className="ranking-title">
                                     <div className="ranking-title-combo">
                                         Filtrar por jogo:
@@ -32,7 +174,7 @@ class employee extends Component{
                                             </FormControl>
                                         </Form>
                                     </div> 
-                                    <div className="ranking-title-title">
+                                    <div className="ranking-title-title-e">
                                         <h2>Ranking</h2>
                                     </div>
                                 </div>
@@ -69,7 +211,7 @@ class employee extends Component{
                                     </table>
                                 </div>
                             </div>
-                            <div className="employee">
+                            {/* <div className="employee">
                                 <div className="employee-title">
                                     <h2>Seu Perfil</h2>
                                 </div>
@@ -89,10 +231,10 @@ class employee extends Component{
                                                 <td><b>Usuário</b></td>
                                                 <td>Matilda53</td>
                                             </tr>
-                                            {/* <tr>
+                                            <tr>
                                                 <td><b>Departamento</b></td>
                                                 <td>Produção</td>
-                                            </tr> */}
+                                            </tr> 
                                             <tr>
                                                 <td><b>CPF</b></td>
                                                 <td>472.522.454-09</td>
@@ -108,13 +250,14 @@ class employee extends Component{
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            </div> */}
                         </div>
                     </center>
                 </React.Fragment>
             </div>
         );
-    };
-};
+ };
 
-export default employee;
+
+
+export default Profile;
