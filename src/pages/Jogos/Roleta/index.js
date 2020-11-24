ad76c345d67d247c036827b9306e2d44d03a3381
Vinicius Prado <vinicavagna@gmail.com>
2020-11-23 23:51:20 -0300

    Resolvendo as coisas que a grazi me pediu

--- src/pages/Jogos/Roleta/index.js
+++ src/pages/Jogos/Roleta/index.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import "../Quiz/style.css";
 import Header from "../../../components/Header";
+import API from "../../../api";
+import Sidebar from "../../../components/Sidebar/employee";
+import SidebarADM from "../../../components/Sidebar/admin";
 import { Helmet } from "react-helmet";
 import "font-awesome/css/font-awesome.min.css";
 import { Container, Row } from "react-bootstrap";
@@ -10,15 +13,41 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 export default class Roleta_rules extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            admin : null
+        };
     }
     
+    componentDidMount(){
+        let tk = {
+            token: localStorage.getItem("authTk"),
+        };
+        API.post(
+            "login/teste-token", 
+            tk
+        ).then((res) => {
+            console.log(res.data.decoded.admin)
+            if(res.data.decoded.admin){
+                console.log("é ademir")
+                this.setState({
+                    admin: <SidebarADM/>
+                });
+            }else{
+                this.setState({
+                    admin: <Sidebar/>
+                });
+            }
+        })
+    }
+
+
     render() {
         return (
             <React.Fragment>
                 <Helmet title="Roleta" />
+                {this.state.admin}
                 <Header headerTitle="Jogo da Roleta"/>
-
+                    
                     <h1 className="titulo_objetivo">Objetivos:</h1>
 
                     <Container className="container_descricao">
@@ -38,11 +67,10 @@ export default class Roleta_rules extends Component {
 
                         <Row style={{paddingRight:"35%"}}>
                             <Container className="play">
-                                <a style={{color:"white", textDecoration:"none"}} href="/jogos/roleta/queue">Jogar</a>
+                                <a style={{color:"white", textDecoration:"none"}} href="/funcionario">Voltar</a>
                             </Container>
-
                             <Container className="play">
-                                <a style={{color:"white", textDecoration:"none"}} href="/">Voltar</a>
+                                <a style={{color:"white", textDecoration:"none"}} href="/jogos/roleta/queue">Jogar</a>
                             </Container>
                         </Row>
                     </Container>
